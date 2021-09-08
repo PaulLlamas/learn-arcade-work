36c64b1ba0c07c33d3982b3a664ed9705301f358
Paul Llamas <paul.llamas@my.simpson.edu>
2021-09-08 11:38:44 -0500

    Work Lab 2

--- Testing/drawing_test.py
+++ Testing/drawing_test.py
@@ -1,3 +1,6 @@
+"""
+Test
+"""
 import arcade
 
 # It opens a window.
@@ -10,22 +13,21 @@ arcade.set_background_color(arcade.csscolor.DEEP_SKY_BLUE)
 arcade.start_render()
 
 # Code will go here.
-#Grass
 arcade.draw_lrtb_rectangle_filled(0, 600, 350, 0, arcade.csscolor.GREENYELLOW)
 
-#First Tree
 arcade.draw_lrtb_rectangle_filled(90, 110, 350, 280, arcade.csscolor.BROWN)
+
 arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
 
-#
 arcade.draw_lrtb_rectangle_filled(200, 220, 350, 280, arcade.csscolor.SIENNA)
 arcade.draw_circle_filled(210, 350, 30, arcade.csscolor.DARK_GREEN)
 
 arcade.draw_lrtb_rectangle_filled(300, 320, 350, 280, arcade.csscolor.SIENNA)
-arcade.draw_ellipse_filled(310, 360, 75, 60, arcade.csscolor.DARK_GREEN)
+arcade.draw_ellipse_filled(300, 300, 50, 50, arcade.csscolor.DARK_GREEN)
 
 
 # Finish drawing.
 arcade.finish_render()
 
 arcade.run()
+
--- Testing/test.py
+++
@@ -1,5 +0,0 @@
-print("""No
-way
-this
-works
-""")
