383bbb88e9198187a4404a87a747690be3e54749
PaulLlamas <paul.llamas@my.simpson.edu>
2021-10-19 11:11:58 -0500

    Lab 7 User Control

---
+++ Lab 07 - User Control/lab_07.py
@@ -0,0 +1,169 @@
+""" Lab 7 - User Control """
+
+import arcade
+
+# --- Constants ---
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 799
+MOVEMENT_SPEED = 10
+
+
+class Strange:
+
+    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
+
+        self.position_x = position_x
+        self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
+        self.radius = radius
+        self.color = color
+
+    def strange_circle_draw(self):
+        arcade.draw_circle_outline(self.position_x - 35, self.position_y - 5, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x - 30, self.position_y - 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x - 25, self.position_y - 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x - 20, self.position_y - 20, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x - 15, self.position_y - 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x - 10, self.position_y - 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x - 5, self.position_y - 5, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x + 5, self.position_y + 5, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x + 10, self.position_y + 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x + 15, self.position_y + 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x + 20, self.position_y + 20, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x + 25, self.position_y + 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x + 30, self.position_y + 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x + 35, self.position_y + 5, self.radius, self.color, 5)
+
+    def update(self):
+
+        self.position_y += self.change_y
+        self.position_x += self.change_x
+
+        if self.position_x < self.radius:
+            self.change_x *= -1
+
+        if self.position_x > SCREEN_WIDTH - self.radius:
+            self.change_x *= -1
+
+        if self.position_y < self.radius:
+            self.change_y *= -1
+
+        if self.position_y > SCREEN_HEIGHT - self.radius:
+            self.change_y *= -1
+
+
+class Part2:
+    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
+        self.radius = radius
+        self.color = color
+
+    def part2_draw(self):
+        arcade.draw_circle_outline(self.position_x, self.position_y - 5, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y - 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y - 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y - 20, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y - 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y - 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y - 5, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y + 5, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y + 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y + 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y + 20, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y + 15, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y + 10, self.radius, self.color, 5)
+        arcade.draw_circle_outline(self.position_x, self.position_y + 5, self.radius, self.color, 5)
+
+    def update(self):
+
+        self.position_y += self.change_y
+        self.position_x += self.change_x
+
+        if self.position_x < self.radius:
+            self.change_x *= -1
+
+        if self.position_x > SCREEN_WIDTH - self.radius:
+            self.change_x *= -1
+
+        if self.position_y < self.radius:
+            self.change_y *= -1
+
+        if self.position_y > SCREEN_HEIGHT - self.radius:
+            self.change_y *= -1
+
+
+class MyGame(arcade.Window):
+
+    def __init__(self):
+
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
+
+        self.set_mouse_visible(False)
+
+        # Create our ball
+        self.circle = Strange(60, 50, 0, 0, 50, arcade.color.BLUSH)
+        self.linear_circle = Part2(60, 50, 0, 0, 50, arcade.color.SILVER)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        # Background
+        arcade.set_background_color(arcade.csscolor.BLACK)
+        arcade.draw_rectangle_outline(130, 130, 50, 200, arcade.csscolor.SNOW, 10, 45)
+        arcade.draw_rectangle_outline(670, 670, 50, 200, arcade.csscolor.SNOW, 10, 45)
+        arcade.draw_rectangle_outline(670, 130, 50, 200, arcade.csscolor.SNOW, 10, 135)
+        arcade.draw_rectangle_outline(130, 670, 50, 200, arcade.csscolor.SNOW, 10, 135)
+        arcade.draw_rectangle_outline(65, 400, 50, 425, arcade.csscolor.SNOW, 10)
+        arcade.draw_rectangle_outline(735, 400, 50, 425, arcade.csscolor.SNOW, 10)
+        arcade.draw_rectangle_outline(400, 735, 50, 425, arcade.csscolor.SNOW, 10, 90)
+        arcade.draw_rectangle_outline(400, 65, 50, 425, arcade.csscolor.SNOW, 10, 90)
+
+        self.circle.strange_circle_draw()
+        self.linear_circle.part2_draw()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.circle.position_x = x
+        self.circle.position_y = y
+
+    def on_mouse_press(self, x, y, button, modifiers):
+
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            print("Left mouse button pressed at", x, y)
+        elif button == arcade.MOUSE_BUTTON_RIGHT:
+            print("Right mouse button pressed at", x, y)
+
+    def update(self, delta_time):
+        self.circle.update()
+        self.linear_circle.update()
+
+    def on_key_press(self, key, modifiers):
+        """ Called whenever the user presses a key. """
+        if key == arcade.key.LEFT:
+            self.linear_circle.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.linear_circle.change_x = MOVEMENT_SPEED
+        elif key == arcade.key.UP:
+            self.linear_circle.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.linear_circle.change_y = -MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.linear_circle.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.linear_circle.change_y = 0
+
+
+def main():
+    window = MyGame()
+    arcade.run()
+
+
+main()
--- Testing/test.py
+++ Testing/test.py
@@ -1,14 +1,1 @@
-class Dog():
-    def __init__(self, new_name):
-        """ Constructor """
-        self.name = new_name
-        print("A dog has been born!")
-
-
-def main():
-    # This creates the dog
-    my_dog = Dog()
-    print(f"The dog's name is: {my_dog.name}")
-
-
-main()
+in range()
