7412a696fb733ec0ef59420e10ccaf9f7bf9819a
Paul Llamas <paul.llamas@my.simpson.edu>
2021-11-18 15:06:16 -0600

    Lab 11

--- Testing/test array.py
+++ Testing/test array.py
@@ -1,5 +1,5 @@
 import arcade
-
+import random
 
 WIDTH = 60
 HIGHT = 60
@@ -20,11 +20,15 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
+        # Create a grid of numbers
         self.grid = []
         for row in range(ROW_COUNT):
             self.grid.append([])
             for column in range(COLUMN_COUNT):
                 self.grid[row].append(0)
+
+        self.grid[2][9] = 1
+
         print(self.grid)
 
     def on_draw(self):
@@ -33,18 +37,28 @@ class MyGame(arcade.Window):
         """
         arcade.start_render()
         for row in range(ROW_COUNT):
-
             for column in range(COLUMN_COUNT):
                 x = WIDTH / 2 + column * (WIDTH + MARGIN) + MARGIN
                 y = HIGHT / 2 + row * (HIGHT + MARGIN) + MARGIN
-                arcade.draw_rectangle_filled(x, y, WIDTH, HIGHT, arcade.color.BABY_BLUE_EYES)
+                if self.grid[row][column] == 0:
+                    color = (random.randrange(256), random.randrange(256), random.randrange(256))
+                else:
+                    color = arcade.color.BABY_BLUE_EYES
+
+                arcade.draw_rectangle_filled(x, y, WIDTH, HIGHT, color)
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """
         Called when the user presses a mouse button.
         """
-        pass
+        row = y // (HIGHT + MARGIN)
+        column = x // (WIDTH + MARGIN)
+        if self.grid[row][column] == 0:
+            self.grid[row][column] = 1
+        else:
+            self.grid[row][column] = 0
 
+        print(f"You clicked on {row} coordinate and {column} coordinate")
 
 def main():
 
